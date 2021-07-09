f90d3fb5def8bb28fe0eb24eacb5439539745b89
Adityan R <adityanr@Adityans-MacBook-Air.local>
2021-07-09 11:45:27 +0530

    updated tables

--- sales/models.py
+++ sales/models.py
@@ -4,5 +4,16 @@ class Item(models.Model):
     item_name = models.CharField(max_length=20)
     item_price = models.IntegerField()
 
+    def __str__(self):
+        return self.item_name
+
 class Sale(models.Model):
-    pass
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.id)
+    
+    def getDate(self):
+        return self.date.strftime('%B/%d/%Y')
--- sales/views.py
+++ sales/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Sale
 
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    sales = Sale.objects.all()
+    context = {'sales' : sales}
+
+    if request.method == 'GET':
+        search_date = request.GET.get('date')
+        print(search_date)
+    return render(request, 'dashboard.html', context)
